af1708624b0e69dc86127c98a6586824b2621129
Artur Zacniewski <a.zacniewski@gmail.com>
2022-03-12 16:38:27 +0100

    Preparing contact page

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index
+from .views import index, contact
 
 app_name = 'frontend'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact')
 ]
