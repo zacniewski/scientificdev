5a9e61afc380ce490cd34891fc34a97e68399546
Artur Zacniewski <a.zacniewski@gmail.com>
2022-03-14 21:10:34 +0100

    PEP8 suggestion

--- blog/views.py
+++ blog/views.py
@@ -45,7 +45,7 @@ def post_detail(request, year, month, day, post):
     similar_posts = Post.published.filter(tags__in=post_tags_ids)\
                                   .exclude(id=post.id)
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
-                                .order_by('-same_tags','-publish')[:4]
+                                .order_by('-same_tags', '-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
