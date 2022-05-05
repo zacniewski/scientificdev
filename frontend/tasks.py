ca9a622ccbab3dea43e1da35abc8df9b3b5d5f5a
Artur Zacniewski <a.zacniewski@gmail.com>
2022-05-05 13:31:09 +0200

    Add title of the Packt book to email

--- frontend/tasks.py
+++ frontend/tasks.py
@@ -1,3 +1,6 @@
+import requests
+
+from bs4 import BeautifulSoup
 from celery.schedules import crontab
 from django.core.mail import EmailMessage
 
@@ -6,9 +9,12 @@ from sd.celery import app
 
 @app.task
 def task_send_email_about_ebook():
+    page = requests.get('https://www.packtpub.com/free-learning')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    book_title = soup.find('h3', attrs={'class': 'product-info__title'}).text
     subject = 'Your free e-book from PacktPub is available!'
-    message = "Your new e-book is"
-    message += " available at <a href='https://www.packtpub.com/packt/offers/free-learning'>Free Learning</a>."
+    message = f"Your new e-book is {book_title} \n"
+    message += f" Book is available at <a href='https://www.packtpub.com/packt/offers/free-learning'>Free Learning</a>."
     email = EmailMessage(subject,
                          message,
                          'artur@scientificdev.net',
@@ -20,6 +26,6 @@ def task_send_email_about_ebook():
 app.conf.beat_schedule = {
     "task_send_email_about_ebook": {
         "task": "frontend.tasks.task_send_email_about_ebook",
-        "schedule": crontab(hour=7, minute=5)
+        "schedule": crontab(hour=13, minute=35)
         }
     }
