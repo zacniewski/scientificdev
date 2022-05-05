a331ce7d4993574f9f9b34266360c38c5af94850
Artur Zacniewski <a.zacniewski@gmail.com>
2022-05-05 13:54:38 +0200

    Refactor title of the free e-book

--- frontend/tasks.py
+++ frontend/tasks.py
@@ -11,9 +11,9 @@ from sd.celery import app
 def task_send_email_about_ebook():
     page = requests.get('https://www.packtpub.com/free-learning')
     soup = BeautifulSoup(page.content, 'html.parser')
-    book_title = soup.find('h3', attrs={'class': 'product-info__title'}).text.split('-', 1)[1]
+    book_title = soup.find('h3', attrs={'class': 'product-info__title'}).text.split('-', 1)[1].strip()
     subject = "Your free e-book from PacktPub is available!"
-    message = f"Your new e-book is {book_title}. \n"
+    message = f"Your new e-book is '{book_title}'. \n"
     message += f"Book is available at <a href='https://www.packtpub.com/packt/offers/free-learning'>Free Learning</a>."
     email = EmailMessage(subject,
                          message,
@@ -26,6 +26,6 @@ def task_send_email_about_ebook():
 app.conf.beat_schedule = {
     "task_send_email_about_ebook": {
         "task": "frontend.tasks.task_send_email_about_ebook",
-        "schedule": crontab(hour=13, minute=50)
+        "schedule": crontab(hour=13, minute=57)
         }
     }
