84a7a3f2d1050ec16cecbf43b224dede676aef01
Artur Zacniewski <a.zacniewski@gmail.com>
2022-06-08 17:05:21 +0200

    Change SQLite DB to PostgreSQL locally

--- settings/base.py
+++ settings/base.py
@@ -79,10 +79,20 @@ WSGI_APPLICATION = 'sd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
 
