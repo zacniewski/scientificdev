b1c64462f5b73ae24e5da321f3e141b2eaeb25ce
Artur Zacniewski <artur.zacniewski@atlasus.com>
2023-02-15 08:52:53 +0100

    Median CHF course according to NBP from bankier.pl

--- frontend/tasks.py
+++ frontend/tasks.py
@@ -45,6 +45,6 @@ app.conf.beat_schedule = {
         },
     "task_send_chf_rate": {
         "task": "frontend.tasks.task_send_chf_rate",
-        "schedule": crontab(hour=8, minute=46)
+        "schedule": crontab(hour=7, minute=10)
         }
     }
