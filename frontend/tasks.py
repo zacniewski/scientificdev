9f36efb40a6b2e6cd2c5efafee022e022ec272a9
Artur Zacniewski <a.zacniewski@gmail.com>
2023-06-25 21:50:13 +0200

    Recuperation filters task corrected date

--- frontend/tasks.py
+++ frontend/tasks.py
@@ -13,39 +13,30 @@ weather_codes = {
     (1,): "głównie bezchmurnie",
     (2,): "częściowo pochmurno",
     (3,): "pochmurno",
-
     (45,): "mgła",
     (48,): "opadająca mgła szronowa",
-
     (51,): "mżawka lekka",
     (53,): "mżawka umiarkowana",
     (55,): "mżawka gęsta",
-
     (56,): "zamrażająca mżawka: lekka",
     (57,): "zamrażająca mżawka: gęsta intensywność",
-
     (61,): "deszcz słaby",
     (63,): "deszcz umiarkowany",
     (65,): "deszcz intensywny",
-
     (66,): "marznący deszcz: intensywność lekka",
     (67,): "marznący deszcz: intensywność ciężka",
-
     (71,): "opady śniegu: intensywność niewielka",
     (73,): "opady śniegu: intensywność umiarkowana",
     (75,): "opady śniegu: intensywność duża",
-
     (77,): "ziarna śniegu",
     (80,): "przelotne opady deszczu: słabe",
     (81,): "przelotne opady deszczu: umiarkowane",
     (82,): "przelotne opady deszczu: gwałtowne",
-
     (85,): "opady śniegu lekkie",
     (86,): "opady śniegu intensywne",
-
     (95,): "burza: Słaba lub umiarkowana",
     (96,): "burza z lekkim gradem",
-    (99,): "burza z silnym gradem"
+    (99,): "burza z silnym gradem",
 }
 
 # Obsługa informacji przypominających o wystawieniu śmieci
@@ -64,7 +55,7 @@ dates_for_trash_set_nr_1 = {
     9: (11,),
     10: (9,),
     11: (6,),
-    12: (4,)
+    12: (4,),
 }
 
 trash_set_nr_2 = ("Odpady zmieszane", "Odpady bio", "Szkło", "Popiół", "Odpady zielone")
@@ -80,37 +71,39 @@ dates_for_trash_set_nr_2 = {
     9: (25,),
     10: (23,),
     11: (20,),
-    12: (18,)
+    12: (18,),
 }
 trash_set_nr_3 = ("Odpady wielkogabarytowe",)
-dates_for_trash_set_nr_3 = {
-    3: (4,),
-    9: (2,)
-}
+dates_for_trash_set_nr_3 = {3: (4,), 9: (2,)}
 
 
 @app.task
 def task_send_email_about_ebook():
-    page = requests.get('https://www.packtpub.com/free-learning')
-    soup = BeautifulSoup(page.content, 'html.parser')
-    book_title = soup.find('h3', attrs={'class': 'product-info__title'}).text.split('-', 1)[1].strip()
+    page = requests.get("https://www.packtpub.com/free-learning")
+    soup = BeautifulSoup(page.content, "html.parser")
+    book_title = (
+        soup.find("h3", attrs={"class": "product-info__title"})
+        .text.split("-", 1)[1]
+        .strip()
+    )
     subject = "Your free e-book from PacktPub is available!"
     message = f"Your new e-book is '{book_title}'. \n"
     message += f"Book is available at <a href='https://www.packtpub.com/packt/offers/free-learning'>Free Learning</a>."
-    email = EmailMessage(subject,
-                         message,
-                         'artur@scientificdev.net',
-                         ['artur.zacniewski@proton.me'])
+    email = EmailMessage(
+        subject, message, "artur@scientificdev.net", ["artur.zacniewski@proton.me"]
+    )
     email.send(fail_silently=False)
 
 
 @app.task
 def task_send_weather_data():
-    weather_data = requests.get('https://api.open-meteo.com/v1/forecast?latitude=54.57&longitude=18.20'
-                                '&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m').json()
+    weather_data = requests.get(
+        "https://api.open-meteo.com/v1/forecast?latitude=54.57&longitude=18.20"
+        "&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
+    ).json()
 
     subject = f"Pogoda na {weather_data['current_weather']['time'].split('T')[0]}"
-    current_weather_code = weather_data['current_weather']['weathercode']
+    current_weather_code = weather_data["current_weather"]["weathercode"]
     current_weather_description = ""
     for key in weather_codes.keys():
         if current_weather_code in key:
@@ -130,10 +123,12 @@ def task_send_weather_data():
         f"- o 18.00: {weather_data['hourly']['relativehumidity_2m'][18]}% \n\n"
         f"IV. Pogoda o 6.00: {current_weather_description}."
     )
-    email = EmailMessage(subject,
-                         message,
-                         'artur@scientificdev.net',
-                         ['artur.zacniewski@proton.me', 'joanna.zacniewska@gmail.com'])
+    email = EmailMessage(
+        subject,
+        message,
+        "artur@scientificdev.net",
+        ["artur.zacniewski@proton.me", "joanna.zacniewska@gmail.com"],
+    )
     email.send(fail_silently=False)
 
 
@@ -150,30 +145,36 @@ def task_trash_reminder():
         for trash in trash_set_nr_1:
             message += f"- {trash} \n"
 
-        email = EmailMessage(subject,
-                             message,
-                             'artur@scientificdev.net',
-                             ['artur.zacniewski@proton.me', 'joanna.zacniewska@gmail.com'])
+        email = EmailMessage(
+            subject,
+            message,
+            "artur@scientificdev.net",
+            ["artur.zacniewski@proton.me", "joanna.zacniewska@gmail.com"],
+        )
         email.send(fail_silently=False)
 
     if number_of_day + 1 in dates_for_trash_set_nr_2.get(number_of_month, (1000,)):
         for trash in trash_set_nr_2:
             message += f"- {trash} \n"
 
-        email = EmailMessage(subject,
-                             message,
-                             'artur@scientificdev.net',
-                             ['artur.zacniewski@proton.me', 'joanna.zacniewska@gmail.com'])
+        email = EmailMessage(
+            subject,
+            message,
+            "artur@scientificdev.net",
+            ["artur.zacniewski@proton.me", "joanna.zacniewska@gmail.com"],
+        )
         email.send(fail_silently=False)
 
     if number_of_day + 1 in dates_for_trash_set_nr_3.get(number_of_month, (1000,)):
         for trash in trash_set_nr_3:
             message += f"- {trash} \n"
 
-        email = EmailMessage(subject,
-                             message,
-                             'artur@scientificdev.net',
-                             ['artur.zacniewski@proton.me', 'joanna.zacniewska@gmail.com'])
+        email = EmailMessage(
+            subject,
+            message,
+            "artur@scientificdev.net",
+            ["artur.zacniewski@proton.me", "joanna.zacniewska@gmail.com"],
+        )
         email.send(fail_silently=False)
 
 
@@ -182,10 +183,9 @@ def task_recuperation_filters():
     subject = "Filtry rekuperatora"
     message = f"Czas na wymianę filtrów rekuperatora. \n"
     message += f"Filtry dostępne są <a href='https://cennik24.pl/'>tutaj</a>."
-    email = EmailMessage(subject,
-                         message,
-                         'artur@scientificdev.net',
-                         ['artur.zacniewski@proton.me'])
+    email = EmailMessage(
+        subject, message, "artur@scientificdev.net", ["artur.zacniewski@proton.me"]
+    )
     email.send(fail_silently=False)
 
 
@@ -193,18 +193,18 @@ def task_recuperation_filters():
 app.conf.beat_schedule = {
     "task_send_email_about_ebook": {
         "task": "frontend.tasks.task_send_email_about_ebook",
-        "schedule": crontab(hour=6, minute=5)
+        "schedule": crontab(hour=6, minute=5),
     },
     "task_send_weather_data": {
         "task": "frontend.tasks.task_send_weather_data",
-        "schedule": crontab(hour=6, minute=0)
+        "schedule": crontab(hour=6, minute=0),
     },
     "task_trash_reminder": {
         "task": "frontend.tasks.task_trash_reminder",
-        "schedule": crontab(hour=19, minute=0)
+        "schedule": crontab(hour=19, minute=0),
     },
     "task_recuperation_filters": {
         "task": "frontend.tasks.task_trash_reminder",
-        "schedule": crontab(day_of_month=24, month_of_year='*/3')
-    }
+        "schedule": crontab(day_of_month=25, month_of_year="*/3", hour=22, minute=0),
+    },
 }
