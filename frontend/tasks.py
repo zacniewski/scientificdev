b0773fa796ea1693718d7c4e6e07fca174909241
Artur Zacniewski <a.zacniewski@gmail.com>
2023-11-22 10:16:53 +0100

    Check look of more weather data

--- frontend/tasks.py
+++ frontend/tasks.py
@@ -205,7 +205,7 @@ app.conf.beat_schedule = {
     },
     "task_send_weather_data": {
         "task": "frontend.tasks.task_send_weather_data",
-        "schedule": crontab(hour=10, minute=15),
+        "schedule": crontab(hour=10, minute=21),
     },
     "task_trash_reminder": {
         "task": "frontend.tasks.task_trash_reminder",
